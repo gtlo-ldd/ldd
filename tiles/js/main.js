3f617ec7484eff2ebfb596ee37ee767ea1e8bc2b
wdizon <ldizon@its.jnj.com>
2020-02-06 19:57:59 +0800

    Added Tiles

---
+++ tiles/js/main.js
@@ -0,0 +1,7 @@
+
+if (window.screen.width < 980 ) {
+window.location = "https://www.figma.com/proto/2t9kyM3Xx55ay6Fum4LEaD/Digital-Learning-Solutions-Mobile?node-id=22%3A0&viewport=-3885%2C278%2C1&scaling=contain";
+}
+else{
+window.location = "https://www.figma.com/proto/1sjYOLrIibtZZTRxqm6Iwv/Digital-Learning-Solutions-Web?node-id=1%3A2&viewport=-692%2C120%2C0.5&scaling=contain";	
+}
