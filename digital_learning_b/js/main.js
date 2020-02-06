e269d810d0340f88a6a53c0975dae0cdb11fc126
wdizon <ldizon@its.jnj.com>
2020-02-06 21:34:44 +0800

    Switched digital learning a and b

--- digital_learning_b/js/main.js
+++ digital_learning_b/js/main.js
@@ -3,5 +3,5 @@ if (window.screen.width < 980 ) {
 window.location = "mobile.html";
 }
 else{
-window.location = "https://rise.articulate.com/share/EtvIwLK6Z_T7l-laYlBMB--CNIJNYhqU#/";	
+window.location = "https://www.figma.com/proto/5oXPSOnrrFc4dxVXWRsz8m/Live-Our-Credo%2C-Know-Our-Code?node-id=18%3A53&viewport=43%2C279%2C0.09111513942480087&scaling=contain";	
 }
