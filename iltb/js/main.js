5fe1afab4b4c40423064ee4239204f9a4065eb51
wdizon <ldizon@its.jnj.com>
2020-02-07 20:17:23 +0800

    Push

---
+++ iltb/js/main.js
@@ -0,0 +1,7 @@
+
+if (window.screen.width < 980 ) {
+window.location = "mobile.html";
+}
+else{
+window.location = "https://gtlo-ldd.github.io/prototype/ilt/ILT_Customer_Excellence_for_Contact_Center.zip";	
+}
