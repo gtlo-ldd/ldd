bf99cbe4b9f81591f08f039197194abf6a344e14
wdizon <ldizon@its.jnj.com>
2020-02-10 17:11:45 +0800

    Push

--- flipbook/js/main.js
+++ flipbook/js/main.js
@@ -3,5 +3,5 @@ if (window.screen.width < 980 ) {
 window.location = "mobile.html";
 }
 else{
-window.location = "https://gtlo-ldd.github.io/prototype/flipbook/flipbook_etime.pdf";	
+window.location = "download.html";	
 }
