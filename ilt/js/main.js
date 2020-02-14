28d47f638e23a66af4c5a696879398354a9020a1
wdizon <ldizon@its.jnj.com>
2020-02-14 22:05:10 +0800

    Push

--- ilt/js/main.js
+++ ilt/js/main.js
@@ -3,5 +3,5 @@ if (window.screen.width < 980 ) {
 window.location = "mobile.html";
 }
 else{
-window.location = "https://gtlo-ldd.github.io/prototype/ilt/ILT_Customer_Excellence_for_Contact_Center.zip";	
+window.location = "https://gtlo-ldd.github.io/prototype/ilt/ILT_Assets.zip";	
 }
