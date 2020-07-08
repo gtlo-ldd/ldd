b05fa9e6c0ea92a2c5f55c68675cb647dd7a60d9
wdizon <ldizon@its.jnj.com>
2020-07-09 00:35:36 +0800

    Learning edge lightbox

---
+++ learning_edge/trigger.js
@@ -0,0 +1,4 @@
+
+function triggerBox() {
+  alert("Certain site features may work improperly if you have an ad blocker turned on. \nConsider disabling it to ensure the best browsing experience.");
+}
