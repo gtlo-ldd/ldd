224b30c5b2a2fad03be105b94217948d8a506560
wdizon <ldizon@its.jnj.com>
2020-08-05 22:27:48 +0800

    Update link
    Updated assets

--- tiles/js/main.js
+++ tiles/js/main.js
@@ -1,9 +1,9 @@
 
 if (window.screen.width < 980 ) {
-window.location = "https://www.figma.com/proto/2t9kyM3Xx55ay6Fum4LEaD/Digital-Learning-Solutions-Mobile?node-id=22%3A0&viewport=-3885%2C278%2C1&scaling=contain";
+window.location = "https://www.figma.com/proto/1sjYOLrIibtZZTRxqm6Iwv/Digital-Learning-Showcase-v1.5?node-id=521%3A1685&viewport=1394%2C886%2C0.3277881443500519&scaling=contain";
 }
 else{
-window.location = "https://www.figma.com/proto/KxvcgCtYFT1npI38d4bO7H/Digital-Learning-Showcase-Web-v1.4?node-id=1%3A2&viewport=123%2C245%2C0.26630330085754395&scaling=contain";	
+window.location = "https://www.figma.com/proto/1sjYOLrIibtZZTRxqm6Iwv/Digital-Learning-Showcase-v1.5?node-id=1%3A2&viewport=163%2C354%2C0.3759003281593323&scaling=contain";	
 }
 
 
