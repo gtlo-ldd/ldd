b48b9c51006a738470a99e94189c0cfe482655d6
wdizon <ldizon@its.jnj.com>
2021-02-16 19:52:31 +0800

    Bulk updates

--- learning_edge/trigger.js
+++
@@ -1,4 +0,0 @@
-
-function triggerBox() {
-  alert("Certain site features may work improperly if you have an ad blocker turned on. \nConsider disabling it to ensure the best browsing experience.");
-}
