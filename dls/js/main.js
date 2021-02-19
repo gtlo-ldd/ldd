b676dcc41bb0263383550159876df13b945ab7cc
wdizon <ldizon@its.jnj.com>
2021-02-19 22:36:15 +0800

    Upload new redirection

---
+++ dls/js/main.js
@@ -0,0 +1,9 @@
+
+if (window.screen.width < 980 ) {
+window.location = "https://www.figma.com/proto/ypx8XU7Q5jywHQQTlhECwh/Digital-Learning-Showcase-v1.6.1?node-id=123%3A72&viewport=-1115%2C-499%2C0.1718011349439621&scaling=contain&hotspot-hints=0&hide-ui=1";
+}
+else{
+window.location = "https://www.figma.com/proto/ypx8XU7Q5jywHQQTlhECwh/Digital-Learning-Showcase-v1.6.1?node-id=123%3A72&viewport=-1115%2C-499%2C0.1718011349439621&scaling=contain&hotspot-hints=0&hide-ui=1";	
+}
+
+
--- tiles/js/main.js
+++ tiles/js/main.js
@@ -1,9 +1,9 @@
 
 if (window.screen.width < 980 ) {
-window.location = "https://www.figma.com/proto/1sjYOLrIibtZZTRxqm6Iwv/Digital-Learning-Showcase-v1.5?node-id=521%3A1685&viewport=1394%2C886%2C0.3277881443500519&scaling=contain";
+window.location = "https://gtlo-ldd.github.io/prototype/tiles/";
 }
 else{
-window.location = "https://www.figma.com/proto/1sjYOLrIibtZZTRxqm6Iwv/Digital-Learning-Showcase-v1.5?node-id=1%3A2&viewport=163%2C354%2C0.3759003281593323&scaling=contain";	
+window.location = "https://gtlo-ldd.github.io/prototype/tiles/";	
 }
 
 
