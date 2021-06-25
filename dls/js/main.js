ff3747651669da7fd7f2cf2331d7542809f1c4b0
Ldizon <ldizon@its.jnj.com>
2021-06-25 20:04:50 +0800

    Revert

---
+++ dls/js/main.js
@@ -0,0 +1,9 @@
+
+if (window.screen.width < 980 ) {
+window.location = "https://www.figma.com/proto/ypx8XU7Q5jywHQQTlhECwh/Digital-Learning-Showcase-v2.0?node-id=832%3A2466&viewport=-10%2C194%2C0.07078957557678223&scaling=scale-down";
+}
+else{
+window.location = "https://www.figma.com/proto/ypx8XU7Q5jywHQQTlhECwh/Digital-Learning-Showcase-v1.6.1?node-id=123%3A72&viewport=-1115%2C-499%2C0.1718011349439621&scaling=contain&hotspot-hints=0&hide-ui=1";	
+}
+
+
