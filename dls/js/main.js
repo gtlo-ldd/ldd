c6742b1aeab4f28621ac4b922b6a007882f65c4f
Ldizon <ldizon@its.jnj.com>
2021-07-28 00:29:47 +0800

    Update

--- dls/js/main.js
+++ dls/js/main.js
@@ -1,9 +1,9 @@
 
 if (window.screen.width < 980 ) {
-window.location = "https://www.figma.com/proto/ypx8XU7Q5jywHQQTlhECwh/Digital-Learning-Showcase-v2.0?node-id=832%3A2466&viewport=-10%2C194%2C0.07078957557678223&scaling=scale-down";
+window.location = "https://www.figma.com/proto/aVPtuWn4w1Q5DvfNTCM4Vi/Digital-Learning-Showcase-v2.1?page-id=168%3A1185&node-id=832%3A2466&viewport=66%2C135%2C0.0697024017572403&scaling=scale-down&starting-point-node-id=832%3A2466";
 }
 else{
-window.location = "https://www.figma.com/proto/ypx8XU7Q5jywHQQTlhECwh/Digital-Learning-Showcase-v1.6.1?node-id=123%3A72&viewport=-1115%2C-499%2C0.1718011349439621&scaling=contain&hotspot-hints=0&hide-ui=1";	
+window.location = "https://www.figma.com/proto/aVPtuWn4w1Q5DvfNTCM4Vi/Digital-Learning-Showcase-v2.1?page-id=65%3A0&node-id=123%3A72&viewport=304%2C119%2C0.056019242852926254&scaling=scale-down&starting-point-node-id=123%3A72";	
 }
 
 
